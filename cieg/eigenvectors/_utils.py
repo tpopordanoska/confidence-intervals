622aa0e8f4dedd93b08819c965ebecb4a279b0ac
Teodora Popordanoska <teodora.popordanoska@gmail.com>
2020-12-27 23:30:43 +0100

    Implement orthogonality constraints

--- cieg/eigenvectors/_utils.py
+++ cieg/eigenvectors/_utils.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import numpy as np
 import torch
 
@@ -86,16 +88,7 @@ def get_eigenvector_bounds(eps, eig, matrix):
         .type(torch.DoubleTensor)
 
     # enforce trivial bounds due to orthonormality
-    v_upper = torch.clamp(v_upper, min=0.0, max=1.0)
-    # We can compute tighter bounds due to L2 norm of vectors equal to one.
-    l2_upper = v_lower * v_lower
-    l2_upper = torch.sqrt(1 - (torch.ones_like(l2_upper).matmul(l2_upper) - l2_upper))
-    v_upper = torch.min(v_upper, l2_upper)
-
-    # We can also computer tighter lower bounds due to L2 norm of vectors equal to one.
-    l2_lower = v_upper * v_upper
-    l2_lower = torch.sqrt(torch.max(torch.zeros_like(l2_lower), 1.0 - (torch.ones_like(l2_lower).matmul(l2_lower) - l2_lower)))
-    v_lower = torch.max(v_lower, l2_lower)
+    v_lower, v_upper = enforce_normality(v_lower, v_upper)
 
     # now get the signed version of the upper and lower bounds on V
     #  cases:
@@ -111,7 +104,136 @@ def get_eigenvector_bounds(eps, eig, matrix):
     tmpinds = np.where(lbind == False)
     v_lower[tmpinds[0], tmpinds[1]] = -v_upper[tmpinds[0], tmpinds[1]]
 
+    print("========= Before orthogonality constraints: =========")
+    print(v_lower)
+    print(v_upper)
+
+    v_lower, v_upper = enforce_orthogonality(v_lower, v_upper)
+    print("========= After orthogonality constraints: =========")
+    print(v_lower)
+    print(v_upper)
+
     return v_lower, v_upper
+
+
+def enforce_normality(v_lower, v_upper):
+    v_upper = torch.clamp(v_upper, min=0.0, max=1.0)
+    # We can compute tighter bounds due to L2 norm of vectors equal to one.
+    l2_upper = v_lower * v_lower
+    l2_upper = torch.sqrt(1 - (torch.ones_like(l2_upper).matmul(l2_upper) - l2_upper))
+    v_upper = torch.min(v_upper, l2_upper)
+
+    # We can also computer tighter lower bounds due to L2 norm of vectors equal to one.
+    l2_lower = v_upper * v_upper
+    l2_lower = torch.sqrt(torch.max(torch.zeros_like(l2_lower), 1.0 - (torch.ones_like(l2_lower).matmul(l2_lower) - l2_lower)))
+    v_lower = torch.max(v_lower, l2_lower)
+
+    return v_lower, v_upper
+
+
+def enforce_orthogonality(v_lower, v_upper):
+    mu_dict, nu_dict = get_bounds_on_sum(v_lower, v_upper)
+    alpha = v_upper
+    beta = v_lower
+    p = v_lower.shape[0]
+    for l in range(p):
+        for i in range(p):
+            for j in range(p):
+                if i == j:
+                    continue
+                mu = get_from_dict((i, j, l), mu_dict)
+                nu = get_from_dict((i, j, l), nu_dict)
+                # case 1
+                if alpha[l, j] < 0:
+                    if beta[l, i] >= 0:
+                        temp_mu = - mu / beta[l, j]
+                        temp_nu = - nu / beta[l, j]
+                    elif alpha[l, i] < 0:
+                        temp_mu = - mu / alpha[l, j]
+                        temp_nu = - nu / alpha[l, j]
+                    else:
+                        temp_mu = torch.min(-mu / alpha[l, j], -mu / beta[l, j])
+                        temp_nu = torch.max(-nu / alpha[l, j], -nu / beta[l, j])
+                    beta[l, i] = torch.max(beta[l, i], temp_mu)
+                    alpha[l, i] = torch.min(alpha[l, i], temp_nu)
+                # case 2
+                elif beta[l, j] > 0:
+                    if beta[l, i] >= 0:
+                        temp_nu = - nu / alpha[l, j]
+                        temp_mu = - mu / alpha[l, j]
+                    elif alpha[l, i] < 0:
+                        temp_nu = - nu / beta[l, j]
+                        temp_mu = - mu / beta[l, j]
+                    else:
+                        temp_mu = torch.min(-mu / alpha[l, j], -mu / beta[l, j])
+                        temp_nu = torch.max(-nu / alpha[l, j], -nu / beta[l, j])
+                    beta[l, i] = torch.max(beta[l, i], temp_nu)
+                    alpha[l, i] = torch.min(alpha[l, i], temp_mu)
+                # case 3
+                elif alpha[l, j] > 0 and beta[l, j] < 0:
+                    if beta[l, i] >= 0:
+                        temp_mu = - mu / beta[l, j]
+                        temp_nu = - nu / alpha[l, j]
+                        beta[l, i] = torch.max(torch.tensor([beta[l, i], temp_mu, temp_nu]))
+                    elif alpha[l, i] < 0:
+                        temp_mu = - mu / alpha[l, j]
+                        temp_nu = - nu / beta[l, j]
+                        alpha[l, i] = torch.min(torch.tensor([alpha[l, i], temp_mu, temp_nu]))
+                # case 4a
+                elif beta[l, j] == 0:
+                    if alpha[l, j] == 0:
+                        continue
+                    if mu > 0:
+                        temp_up = - mu / alpha[l, j]
+                        alpha[l, i] = torch.min(alpha[l, i], temp_up)
+                    if nu < 0:
+                        temp_low = - nu / alpha[l, j]
+                        beta[l, i] = torch.max(beta[l, i], temp_low)
+                # case 4b
+                elif alpha[l, j] == 0:
+                    if beta[l, j] == 0:
+                        continue
+                    if mu > 0:
+                        temp_up = - mu / beta[l, j]
+                        alpha[l, i] = torch.min(alpha[l, i], temp_up)
+                    if nu < 0:
+                        temp_low = - nu / alpha[l, j]
+                        beta[l, i] = torch.max(beta[l, i], temp_low)
+
+    return beta, alpha
+
+
+def get_bounds_on_sum(v_lower, v_upper):
+    mu_dict = defaultdict(int)
+    nu_dict = defaultdict(int)
+    p = v_lower.shape[0]
+
+    for l in range(p):
+        for i in range(p - 1):
+            for j in range(i + 1, p):
+                for k in range(p):
+                    if k == l:
+                        continue
+
+                    low_low = v_lower[k, i] * v_lower[k, j]
+                    low_up = v_lower[k, i] * v_upper[k, j]
+                    up_low = v_upper[k, i] * v_lower[k, j]
+                    up_up = v_upper[k, i] * v_upper[k, j]
+
+                    mu_dict[(i, j, l)] += torch.min(torch.tensor([low_low, low_up, up_low, up_up]))
+                    nu_dict[(i, j, l)] += torch.max(torch.tensor([low_low, low_up, up_low, up_up]))
+
+    return mu_dict, nu_dict
+
+
+def get_from_dict(key, dictionary):
+    i, j, l = key
+    if key in dictionary:
+        return dictionary[key]
+    elif (j, i, l) in dictionary:
+        return dictionary[j, i, l]
+    else:
+        print(f"No element with key {i, j, l} found in dictionary.")
 
 
 def print_eig_bounds(eigvals_lower, eigvals_upper, eigvects_lower, eigvects_upper, eig):
